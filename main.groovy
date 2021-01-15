111bb6a0da30a223f8ce7a49318070b4ae620371
Atis <atis@aol.es>
2021-01-16 00:48:12 +0200

    fix typo; add comment

--- main.groovy
+++ main.groovy
@@ -2,8 +2,9 @@
 
 FrequencyCounter fc = new FrequencyCounter()
 
-// read text from file and analyize it
+// read text from file and analyze it
 String text = new File('./bta-news.txt').getText('UTF-8')
 fc.processText(text)
 
+// print character counts to stdout
 println fc
